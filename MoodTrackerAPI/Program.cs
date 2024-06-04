4cc1cdec1c8c53589a0538887679a2973de6ca6f
i463444 <463444@student.fontys.nl>
2024-06-04 16:16:37 +0200

    add grafana code

--- MoodTrackerAPI/Program.cs
+++ MoodTrackerAPI/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using MoodTrackingService.Data;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,16 @@ builder.Services.AddScoped<MongoDbContext>(provider =>
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// OpenTelemetry Configuration
+builder.Services.AddOpenTelemetryTracing(b => b
+    .AddAspNetCoreInstrumentation()
+    .AddHttpClientInstrumentation()
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MoodTrackingService"))
+    .AddGenevaExporter(o =>
+    {
+        o.ConnectionString = "InstrumentationKey=your-instrumentation-key";
+    }));
 
 var app = builder.Build();
 
