9f801f4d7aebacf35bc80fa5644a5fa0c6e5f26c
i463444 <463444@student.fontys.nl>
2024-06-08 16:26:33 +0200

    added new services

---
+++ Services/SupportiveMessageConsumer/Data/MessagesContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+public class MessagesContext : DbContext
+{
+    public MessagesContext(DbContextOptions<MessagesContext> options) : base(options) { }
+
+    public DbSet<SupportiveMessage> SupportiveMessages { get; set; }
+}
+
+public class SupportiveMessage
+{
+    public int Id { get; set; }
+    public string Message { get; set; }
+}
---
+++ Services/SupportiveMessageConsumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+var services = new ServiceCollection();
+services.AddDbContext<MessagesContext>(options =>
+    options.UseNpgsql("Host=postgresql;Database=messagesdb;Username=postgres;Password=password"));
+var serviceProvider = services.BuildServiceProvider();
+
+var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare(queue: "supportive_messages", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[x] Received {message}");
+
+    using var scope = serviceProvider.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<MessagesContext>();
+    context.SupportiveMessages.Add(new SupportiveMessage { Message = message });
+    context.SaveChanges();
+};
+
+channel.BasicConsume(queue: "supportive_messages", autoAck: true, consumer: consumer);
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
