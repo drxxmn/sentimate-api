214cba5b9c2ed6d8d3554cf671fadefa3f5fdaca
i463444 <463444@student.fontys.nl>
2024-06-08 18:15:56 +0200

    fix path

--- Services/SupportiveMessageProducer/Controllers/MessageController.cs
+++ Services/SupportiveMessageProducer/Controllers/MessageController.cs
@@ -2,21 +2,24 @@ using Microsoft.AspNetCore.Mvc;
 using RabbitMQ.Client;
 using System.Text;
 
-[Route("api/[controller]")]
-[ApiController]
-public class MessageController : ControllerBase
+namespace SupportiveMessageProducer.Controllers
 {
-    [HttpPost]
-    public IActionResult Post([FromBody] string message)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
     {
-        var factory = new ConnectionFactory() { HostName = "rabbitmq" };
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
-        channel.QueueDeclare(queue: "supportive_messages", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        [HttpPost]
+        public IActionResult Post([FromBody] string message)
+        {
+            var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "supportive_messages", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-        var body = Encoding.UTF8.GetBytes(message);
-        channel.BasicPublish(exchange: "", routingKey: "supportive_messages", basicProperties: null, body: body);
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "", routingKey: "supportive_messages", basicProperties: null, body: body);
 
-        return Ok("Message sent to RabbitMQ");
+            return Ok("Message sent to RabbitMQ");
+        }
     }
 }
