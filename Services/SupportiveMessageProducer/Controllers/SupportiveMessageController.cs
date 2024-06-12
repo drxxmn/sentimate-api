92abcbd6d50ffa0ad4e3f8e8bd6f95b6d7ec042f
i463444 <463444@student.fontys.nl>
2024-06-12 19:49:00 +0200

    update appsettings

--- Services/SupportiveMessageProducer/Controllers/SupportiveMessageController.cs
+++ Services/SupportiveMessageProducer/Controllers/SupportiveMessageController.cs
@@ -1,32 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
-using RabbitMQ.Client;
-using System.Text;
-
-namespace SupportiveMessageProducer.Controllers
+[ApiController]
+[Route("[controller]")]
+public class SupportiveMessageController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class SupportiveMessageController : ControllerBase
+    private readonly RabbitMQPublisher _rabbitMQPublisher;
+
+    public SupportiveMessageController(RabbitMQPublisher rabbitMQPublisher)
     {
-        private readonly RabbitMQPublisher _rabbitMQPublisher;
-
-        public SupportiveMessageController(RabbitMQPublisher rabbitMQPublisher)
-        {
-            _rabbitMQPublisher = rabbitMQPublisher;
-        }
-
-        [HttpPost]
-        public IActionResult SendSupportiveMessage([FromBody] SupportiveMessage message)
-        {
-            _rabbitMQPublisher.PublishMessage(message);
-            return Ok("Message sent to queue");
-        }
+        _rabbitMQPublisher = rabbitMQPublisher;
     }
 
-    public class SupportiveMessage
+    [HttpPost]
+    public IActionResult SendSupportiveMessage([FromBody] SupportiveMessage message)
     {
-        public string Content { get; set; }
-        public string Sender { get; set; }
-        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        _rabbitMQPublisher.PublishMessage(message);
+        return Ok("Message sent to queue");
     }
+}
+
+public class SupportiveMessage
+{
+    public string Content { get; set; }
+    public string Sender { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
 }
--- Services/SupportiveMessageProducer/Services/RabbitMQPublisher.cs
+++ Services/SupportiveMessageProducer/Services/RabbitMQPublisher.cs
@@ -6,16 +6,25 @@ public class RabbitMQPublisher
 {
     private readonly string _hostName;
     private readonly string _queueName;
+    private readonly string _username;
+    private readonly string _password;
 
     public RabbitMQPublisher(IConfiguration configuration)
     {
         _hostName = configuration["RabbitMQ:Host"];
         _queueName = configuration["RabbitMQ:QueueName"];
+        _username = configuration["RabbitMQ:Username"];
+        _password = configuration["RabbitMQ:Password"];
     }
 
     public void PublishMessage(object message)
     {
-        var factory = new ConnectionFactory() { HostName = _hostName };
+        var factory = new ConnectionFactory() 
+        { 
+            HostName = _hostName,
+            UserName = _username,
+            Password = _password
+        };
         using var connection = factory.CreateConnection();
         using var channel = connection.CreateModel();
         channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
