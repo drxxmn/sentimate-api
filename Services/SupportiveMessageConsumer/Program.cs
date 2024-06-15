78b3f465f9576cd4cc0e12df4732908d0d24ac07
i463444 <463444@student.fontys.nl>
2024-06-15 19:24:55 +0200

    update logging start database connection

--- Services/SupportiveMessageConsumer/Program.cs
+++ Services/SupportiveMessageConsumer/Program.cs
@@ -1,22 +1,46 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using SupportiveMessageConsumer.Data;
 using SupportiveMessageConsumer.Services;
+using MongoDB.Driver;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddSingleton<MongoDbContext>();
-builder.Services.AddSingleton<RabbitMQConsumer>();
-builder.Services.AddHostedService<RabbitMQConsumer>();
-builder.Services.AddLogging(logging =>
+public class Program
 {
-    logging.ClearProviders();
-    logging.AddConsole();
-});
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
 
-var app = builder.Build();
+        TestDatabaseConnection(host);
 
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+        host.Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+
+    private static void TestDatabaseConnection(IHost host)
+    {
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                var dbContext = services.GetRequiredService<MongoDbContext>();
+                var collection = dbContext.SupportiveMessages;
+                logger.LogInformation("Successfully connected to MongoDB and accessed the SupportiveMessages collection.");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while testing the database connection.");
+            }
+        }
+    }
+}
